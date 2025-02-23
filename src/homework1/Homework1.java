4a4e9512bbeb9ecf159b38bda17abcf06a103d17
debilenok420 <danilasapegin173@gmail.com>
2025-02-23 20:00:45 +0300

    homework1

---
+++ src/homework1/Homework1.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int b = 5; // 1 usage
+        int c = 3; // 1 usage
+        int s = 4 * (b + c - 1) / 2; // 1 usage
+        System.out.println(s);
+
+    }
+}
---
+++ src/homework2/Homework2.java
@@ -0,0 +1,11 @@
+package homework2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        int fullnumber = 68;
+        int tensdigit = fullnumber / 10;
+        int units = fullnumber % 10;
+        int sum = tensdigit + units;
+        System.out.println(sum);
+    }
+}
